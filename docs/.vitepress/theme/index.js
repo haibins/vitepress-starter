814c02b61bb4e9e3e417998f0f8bd6aa16c88d4f
haibin <673183633@qq.com>
2023-08-09 19:50:11 +0800

    feat: 修改复现版本

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,10 +1,13 @@
-import DefaultTheme from "vitepress/theme";
-import { Sandbox } from "vitepress-plugin-sandpack";
+import DefaultTheme from 'vitepress/theme';
+import { Sandbox } from 'vitepress-plugin-sandpack';
+import 'vitepress-plugin-sandpack/dist/style.css';
+
+
 
 export default {
-  ...DefaultTheme,
-  enhanceApp(ctx) {
-    DefaultTheme.enhanceApp(ctx);
-    ctx.app.component("Sandbox", Sandbox);
-  },
+    ...DefaultTheme,
+    enhanceApp(ctx) {
+        DefaultTheme.enhanceApp(ctx);
+        ctx.app.component('Sandbox', Sandbox);
+    },
 };
